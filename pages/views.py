da8cd014d6c86fd79cabf801d1a613f1391ae626
abhishekangira <abhishek_music@yahoo.com>
2018-07-14 03:13:11 +0530

    Added Templates

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    data = {
+    'text':'Very important text'
+    }
+    return render(request, 'home.html', data)
+    # return HttpResponse('<h1>Hello ' + str(request.user) + '!!!</h1><h2> We Love You :)</h2>')
+
+def contact_view(request, *args, **kwargs):
+    return render(request, 'contact.html', {})
+    # return HttpResponse('<h1>Contact me ' + str(request.user) + ', please...</h1><h3>P.S. We Love You :)</h2>')
--- songs/models.py
+++ songs/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Song(models.Model):
-   title  = models.CharField(max_length=100)
-   album  = models.CharField(max_length=60)
-   year   = models.IntegerField(blank=True, null=True)
-   lyrics = models.TextField(default="Shitty Lyrics")
+   title    = models.CharField(max_length=100)
+   album    = models.CharField(max_length=60)
+   year     = models.IntegerField(blank=True, null=True)
+   lyrics   = models.TextField(default="Shitty Lyrics")
+   featured = models.BooleanField(default=False)
